d4f12556afda5170a3638f70a4501330dc8138f7
sashikant12 <86918437+sashikant12@users.noreply.github.com>
2023-09-10 14:03:37 +0530

    Add files via upload

---
+++ FxTrading/src/main/java/com/Finzly/FxTrading/FxTradingApplication.java
@@ -0,0 +1,13 @@
+package com.Finzly.FxTrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxTradingApplication.class, args);
+	}
+
+}
